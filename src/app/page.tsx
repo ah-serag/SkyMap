6ff721391d0fabd1da40311a47dd1adcc286240f
unknown <aserag877@.gmailcom>
2025-08-22 00:53:47 +0300

    delete pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
 import React from 'react'
-import { Button } from './components/ui/button'
-import Link from './components/Link/Link'
 
 const page = () => {
   return (
     <div className='flex h-screen items-center justify-center'>
          
-       <div className=' flex gap-2'>
-      <Button variant='default' size={'lg'} >
-        <Link href='/login'>
-          signIn
-        </Link>
-      </Button>
-      <Button variant='default' size={'lg'}>
-        <Link href='/signUp'>
-          signUp
-        </Link>
-      </Button>
-       </div>
-
-    
-
+      <h1>hello</h1>
     </div>
   )
 }
